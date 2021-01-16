afff4f712d7515d74837fc77276ccbf4326a745c
Eduardo Rico <17030434@itcelaya.edu.mx>
2021-01-15 21:35:25 -0600

    v1

--- db/database.sql
+++ db/database.sql
@@ -3,6 +3,28 @@
 CREATE DATABASE IF NOT EXISTS company;
 
 USE company;*/
+
+drop table servicios_medicos;
+drop table pacientes_enfermedades;
+drop table recetas_medicamentos;
+drop table pacientes_cirugias;
+drop table ventas_medicamentos;
+drop table consultas_media;
+drop table carrito;
+drop table pacientes_alergias;
+drop table recetas;
+drop table medicamentos;
+drop table covid;
+drop table consultas;
+drop table ventas;
+drop table pacientes;
+drop table enfermedades;
+drop table alergias;
+drop table cirugias;
+drop table medicos;
+drop table especialidades;
+drop table servicios;
+
 create table especialidades(
     id integer auto_increment primary key,
     descripcion varchar(60)
@@ -245,25 +267,25 @@ insert into recetas(idConsulta, idMedico, descripcion) values
 
 insert into medicamentos(nombre, descripcion, costo, foto) values
 ('Simvastatina','Se emplea para reducir el colesterol y los triglicéridos (tipo de grasa) en la sangre. Descubierta y desarrollada por Merck, se trata del primer medicamento con estatina que evidenció una disminución de la enfermedad cardiovascular y mortalidad.'
-,300,'url'),
+,300,'Simvastatina.png'),
 ('Aspirina','También conocida como ácido acetil-salicílico (ASA), reduce las sustancias en el cuerpo que producen dolor, fiebre e inflamación. Es el medicamento más usado en todo el mundo y se calcula que cada año se consumen unas 40.000 toneladas. A veces incluso se usa como tratamiento o prevención de infartos de miocardio, derrames cerebrales y dolores en el pecho, y también puede ser efectiva en la prevención de ciertos tipos de cáncer, en particular, el cáncer colorectal. '
-,100,'url'),
+,100,'Aspirina.png'),
 ('Omeprazol','Para la acidez de estómago inhibe la bomba de protones (IBPS)y disminuye la producción de ácido al bloquear la enzima de la pared del estómago que se encarga de producir esta sustancia. Este efecto reviene las úlceras y tiene un resultadocu rativo sobre las úlceras existentes en el esófago, estómago y duodeno. La OMS lo incluye como medicamento básico.'
-,50,'url'),
+,50,'Omeprazol.png'),
 ('Lexotiroxina sódica','Se encarga de sustituir una hormona que se suele producir en nuestra glándula tiroidea para regular la energía y el metabolismo del cuerpo. Es una versión artificial de la hormona tiroxina, responsable de aumentar la tasa metabólica de las células de todos los tejidos del organismo y ayuda a mantener la función cerebral, la absorción de los alimentos y la temperatura corporal, entre otros efectos. '
-,75,'url'),
+,75,'Lexotiroxina.png'),
 ('Ramipril','Para la hipertensión, trata la presión arterial alta (hipertensión) o la insuficiencia cardíaca congestiva. También mejora la supervivencia después de un infarto de miocardio y previene la insuficiencia renal por presión alterial alta y diabetes.'
-,122,'url'),
+,122,'Ramipril.png'),
 ('Amlodipina','Para la hipertensión y la angina, es un bloqueador de los canales de calcio. Ensancha los vasos sanguíneos y mejora el flujo de la sangre, por lo que se usa para reducir la presión arterial y tratar la hipertensión. Ralentizan el latido cardíaco y, al bloquear la señal de calcio en las células de la corteza adrenal, disminuyen la presión arterial.'
-,34,'url'),
+,34,'Amlodipina.png'),
 ('Paracetamol','Para aliviar el dolor, es un medicamento ampliamente empleado para reducir la fiebre, aunque a día de hoy aún se desconoce su mecanismo de acción exacto. Se usa para tratar diversas dolencias como fiebres, dolor de cabeza, dolores musculares, artritis, dolor de espalda o resfriados. Aunque es seguro si se siguen las dosis recomendadas, sobredosis pequeñas pueden causar hasta la muerte. El paracetamol es más tóxico en sobredosis que otros medicamentos pero menos cuando su toma se realiza de forma crónica a pequeñas dosis.'
-,30,'url'),
+,30,'Paracetamol.png'),
 ('Atorvastatina','Para controlar el colesterol, disminuye la cantidad de colesterol que fabrica el hígado. Sirve para reducir los niveles de trigliricéridos en sangre y colesterol ''malo'', al tiempo que aumenta los niveles de colesterol bueno. Se suele emplear junto a una dieta saludable para tratar el colesterol y disminuir el riesgo de derrame cerebral o infarto de miocardio.'
-,72,'url'),
+,72,'Atorvastatina.png'),
 ('Salbutamol','Para el asma. Popularmente conocido como Ventolin, se usa como prevención de broncoespasmos en pacientes con asma, bronquitis, enfisema y otras enfermedades del pulmón. Alivia la tos, la falta de aire y la respiración dificultosa al aumentar el flujo de aire que pasa a través de los tubos bronquiales.'
-,90,'url'),
+,90,'Salbutamol.png'),
 ('Lansoprazol','Para controlar el ácido del estómago, se encarga también de disminuir la cantidad de ácido producido en el estómago y se usa para tratar y prevenir las úlceras en este órgano y en el intestino y para controlar el ardor.'
-,88,'url');
+,88,'Lansoprazol.png');
 
 insert into recetas_medicamentos(idRecetas, idMedicamentos) values
 (1,1),(1,2),(1,3),(1,4),(2,5),(2,6),(2,7),(2,8);
